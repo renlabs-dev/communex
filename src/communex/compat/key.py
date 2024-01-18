96977667f71ffbb65e447befe97fbb3f8057be63
Kelvin Steiner <me@steinerkelvin.dev>
2024-01-18 19:21:02 -0300

    add key code

--- src/communex/compat/key.py
+++ src/communex/compat/key.py
@@ -1,5 +1,43 @@
 """
-Key storage compatible with the `commune` library.
+Key storage compatible with the classic `commune` library.
 
 WIP
 """
+
+from typing import Any, cast
+
+from cexpl.types import CommuneKeyDict
+from communex.key import is_ss58_address
+
+
+def check_key_dict(key_dict: Any) -> CommuneKeyDict:
+    """
+    Validates a given dictionary as a commune key dictionary and returns it.
+
+    This function checks if the provided dictionary adheres to the structure of
+    a CommuneKeyDict, that is used by the classic `commune` library and returns
+    it if valid.
+
+    Args:
+        key_dict: The dictionary to validate.
+
+    Returns:
+        The validated commune key dictionary. Same as input.
+
+    Raises:
+      AssertionError: If the dictionary does not conform to the expected
+        structure.
+    """
+
+    assert isinstance(key_dict, dict)
+    assert isinstance(key_dict["crypto_type"], int)
+    assert isinstance(key_dict["seed_hex"], str)
+    assert isinstance(key_dict["derive_path"], str | None)
+    assert isinstance(key_dict["path"], str)
+    assert isinstance(key_dict["public_key"], str)
+    assert isinstance(key_dict["ss58_format"], int)
+    assert isinstance(key_dict["ss58_address"], str)
+    assert is_ss58_address(key_dict["ss58_address"])
+    assert isinstance(key_dict["private_key"], str)
+    assert isinstance(key_dict["mnemonic"], str)
+    return cast(CommuneKeyDict, key_dict)
--- src/communex/key.py
+++ src/communex/key.py
@@ -1,5 +1,41 @@
 # TODO: move `is_valid_ss58_address` and `check_ss58_address`
 
+from typing import TypeGuard
 
-def is_ss58_address():
-    pass
+from substrateinterface.utils import ss58  # type: ignore
+
+from cexpl.types import Ss58Address
+
+
+def is_ss58_address(address: str, ss58_format: int = 42) -> TypeGuard[Ss58Address]:
+    """
+    Validates whether the given string is a valid SS58 address.
+
+    Args:
+        address: The string to validate.
+        ss58_format: The SS58 format code to validate against.
+
+    Returns:
+        True if the address is valid, False otherwise.
+    """
+
+    return ss58.is_valid_ss58_address(address, valid_ss58_format=ss58_format)
+
+
+def check_ss58_address(address: str | Ss58Address, ss58_format: int = 42) -> Ss58Address:
+    """
+    Validates whether the given string is a valid SS58 address.
+
+    Args:
+        address: The string to validate.
+        ss58_format: The SS58 format code to validate against.
+
+    Returns:
+        The validated SS58 address.
+
+    Raises:
+        AssertionError: If the address is invalid.
+    """
+
+    assert is_ss58_address(address, ss58_format), f"Invalid SS58 address '{address}'"
+    return Ss58Address(address)
