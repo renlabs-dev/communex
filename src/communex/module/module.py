31637f5be2ae4e6788f287a350c47d86b07d1cf6
Kelvin Steiner <me@steinerkelvin.dev>
2024-03-06 16:17:27 -0300

    exposing methods on FastAPI

--- src/communex/module/module.py
+++ src/communex/module/module.py
@@ -3,25 +3,42 @@ Tools for defining Commune modules.
 """
 
 import inspect
-from typing import Any, Callable, ParamSpec, TypeVar, cast
+from dataclasses import dataclass
+# from functools import wraps
+from typing import Any, Callable, Generic, ParamSpec, TypeVar, cast
 
+import fastapi
 import pydantic
+import uvicorn
 from pydantic import BaseModel
 
 T = TypeVar('T')
 P = ParamSpec('P')
 
 
+
+@dataclass
+class EndpointDefinition(Generic[T, P]):
+    name: str
+    fn: Callable[P, T]
+    params_model: type[BaseModel]
+
+
 def endpoint(fn: Callable[P, T]) -> Callable[P, T]:
     sig = inspect.signature(fn)
     model = function_params_to_model(sig)
-    print(model)
+    name = fn.__name__
 
-    def wrapper(*args: P.args, **kwargs: P.kwargs):
-        fn_return = fn(*args, **kwargs)
-        return fn_return
+    endpoint_def = EndpointDefinition(name, fn, model)
 
-    return wrapper
+    # @wraps(fn)
+    # def wrapper(*args: P.args, **kwargs: P.kwargs):
+    #     answer = fn(*args, **kwargs)
+    #     return answer
+
+    fn._endpoint_def = endpoint_def  # type: ignore
+
+    return fn
 
 
 def function_params_to_model(signature: inspect.Signature) -> type[BaseModel]:
@@ -47,5 +64,46 @@ def function_params_to_model(signature: inspect.Signature) -> type[BaseModel]:
 
 
 class Module:
-    pass
+    def __init__(self) -> None:
+        self.endpoints = self.extract_endpoints()
 
+    def extract_endpoints(self):
+        endpoints: dict[str, EndpointDefinition[Any, Any]] = {}
+        for name, method in inspect.getmembers(self, predicate=inspect.ismethod):
+            if hasattr(method, '_endpoint_def'):
+                endpoint_def: EndpointDefinition = method._endpoint_def  # type: ignore
+                endpoints[name] = endpoint_def  # type: ignore
+        return endpoints
+
+
+class ModuleServer:
+    def __init__(self, module: Module) -> None:
+        self._module = module
+        self._app = fastapi.FastAPI()
+        self.register_endpoints()
+
+    def get_fastapi_app(self):
+        return self._app
+
+    def register_endpoints(self):
+        for name, endpoint_def in self._module.endpoints.items():
+            class Body(BaseModel):
+                params: endpoint_def.params_model  # type: ignore
+            def handler(body: Body):
+                return endpoint_def.fn(self._module, **body.params.model_dump())  # type: ignore
+            self._app.post(f"/{name}")(handler)
+
+
+
+if __name__ == "__main__":
+
+    class Amod(Module):
+        @endpoint
+        def do_the_thing(self, awesomness: int = 42):
+            return {"msg": f"Level of awesomness: {awesomness}"}
+
+    a_mod = Amod()
+    server = ModuleServer(a_mod)
+    app = server.get_fastapi_app()
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)  # type: ignore
