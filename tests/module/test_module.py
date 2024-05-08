2d5bea61d344f4082b34469ee5e59404f0367cac
Cipriano <gabrielfcipriano@gmail.com>
2024-05-08 12:35:11 +0200

    tests: module

---
+++ tests/module/test_module.py
@@ -0,0 +1,21 @@
+from communex.module.module import Module, endpoint, EndpointDefinition
+
+
+class SomeModule(Module):
+    @endpoint
+    def get_uppercased(self, msg: str):
+        return {"msg": msg.upper()}
+
+
+def test_module_get_endpoints():
+    a_module = SomeModule()
+
+    endpoints = a_module.get_endpoints()
+
+    assert "get_uppercased" in endpoints
+
+    assert isinstance(endpoints["get_uppercased"], EndpointDefinition)
+
+    assert callable(endpoints['get_uppercased'].fn)
+
+    assert endpoints['get_uppercased'].fn(a_module, msg="example") == {"msg": "EXAMPLE"}
