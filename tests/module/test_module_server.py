0a09efad9afe078258e1db1a28176a6ae06dcb02
Cipriano <gabrielfcipriano@gmail.com>
2024-05-12 22:02:40 +0200

    fix: duplicated test

--- tests/module/test_module_server.py
+++ tests/module/test_module_server.py
@@ -83,7 +83,6 @@ def test_module_server_call_success(serve, server_keypair: Keypair, my_keypair: 
     
     result = requests.post(method_endpoint, headers=headers, json=json.loads(serialized_data))
     
-    # assert result.status_code == 100
     assert result.status_code == 200
     assert result.json() == {'output': 'An answer example for the prompt "all good?"'}
     
@@ -100,29 +99,8 @@ def test_module_server_call_invalid_fn(serve, server_keypair: Keypair, my_keypai
     
     result = requests.post(method_endpoint, headers=headers, json=json.loads(serialized_data))
     
-    # assert result.status_code == 100
     assert result.status_code == 404
     assert result.json() == {'detail': 'Not Found'}
-    
-
-def test_module_server_call_blacklisted(serve, server_keypair: Keypair, my_keypair: Keypair, some_module_server: ModuleServer):
-    server_ss58_address = check_ss58_address(server_keypair.ss58_address)
-    some_module_server.add_to_blacklist(my_keypair.ss58_address)
-    
-    method_endpoint = module_endpoint("prompt")
-    params = {
-        "msg": "all good?"
-    }
-    
-    serialized_data, headers = create_request_data(my_keypair, target_key=server_ss58_address, params=params)
-    
-    result = requests.post(method_endpoint, headers=headers, json=json.loads(serialized_data))
-    
-    # assert result.status_code == 100
-    assert result.status_code == 403
-    assert result.json() == {"error": {"code": 403, "message": "You are blacklisted"}}
-
-
 
 
 def test_module_server_call_blacklisted(serve, server_keypair: Keypair, some_module_server: ModuleServer):
@@ -142,6 +120,25 @@ def test_module_server_call_blacklisted(serve, server_keypair: Keypair, some_mod
     
     assert result.status_code == 403
     assert result.json() == {"error": {"code": 403, "message": "You are blacklisted"}}
+
+
+def test_module_server_call_not_blacklisted(serve, server_keypair: Keypair, some_module_server: ModuleServer):
+    server_ss58_address = check_ss58_address(server_keypair.ss58_address)
+    
+    my_keypair = random_keypair()
+    some_module_server.add_to_blacklist("some other address")
+    
+    method_endpoint = module_endpoint("prompt")
+    params = {
+        "msg": "all good?"
+    }
+    
+    serialized_data, headers = create_request_data(my_keypair, target_key=server_ss58_address, params=params)
+    
+    result = requests.post(method_endpoint, headers=headers, json=json.loads(serialized_data))
+    
+    assert result.status_code == 200
+    assert result.json() == {'output': 'An answer example for the prompt "all good?"'}
 
 
 def test_module_server_call_whitelisted(serve, server_keypair: Keypair, my_keypair: Keypair, some_module_server: ModuleServer):
