549eb5dfbc31d055f66d026cfe3478050d290766
PsicoThePato <jmomcontato@gmail.com>
2024-05-18 16:11:09 -0300

    :fire: removing unused import

--- src/communex/cli/balance.py
+++ src/communex/cli/balance.py
@@ -6,7 +6,7 @@ from communex._common import BalanceUnit, format_balance
 from communex.balance import to_nano
 from communex.cli._common import (make_custom_context,
                                   print_table_from_plain_dict)
-from communex.compat.key import try_classic_load_key, resolve_key_ss58, resolve_key_ss58_encrypted
+from communex.compat.key import try_classic_load_key, resolve_key_ss58_encrypted
 from communex.errors import ChainTransactionError
 
 balance_app = typer.Typer(no_args_is_help=True)
